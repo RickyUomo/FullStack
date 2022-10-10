ccc4e6c8a0a405c0fe9cdc6f95f5915ff01ba080
Ricky Kuo <rickykuo925@gmail.com>
2022-10-10 14:35:21 +0100

    more api test

--- part4/blog/tests/node_api.test.js
+++ part4/blog/tests/node_api.test.js
@@ -1,14 +1,26 @@
 const mongoose = require('mongoose');
-const supertest = require('supertest');
+const superagent = require('supertest');
 const app = require('../app');
 
-const api = supertest(app);
+const api = superagent(app);
 
-test('notes are returned as json', async () => {
+test('blogs are returned as json', async () => {
     await api.get('/api/blogs')
         .expect(200)
         .expect('Content-Type', /application\/json/);
 }, 100000);
+
+test('blogs have 2 instances', async () => {
+    const response = await api.get('/api/blogs');
+
+    expect(response.body).toHaveLength(2);
+});
+
+test('the first note is about HTTP methods', async () => {
+    const response = await api.get('/api/blogs');
+
+    expect(response.body[0].title).toBe('cool guy');
+});
 
 afterAll(() => {
     mongoose.connection.close();
