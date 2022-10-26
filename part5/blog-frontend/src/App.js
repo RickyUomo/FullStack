6ca6934662089d6d90af8e809aca94f7ebba5b6b
Ricky Kuo <rickykuo925@gmail.com>
2022-10-26 15:17:03 +0100

    login and show blogs

--- part5/blog-frontend/src/App.js
+++ part5/blog-frontend/src/App.js
@@ -1,24 +1,107 @@
-import { useState, useEffect } from 'react'
-import Blog from './components/Blog'
-import blogService from './services/blogs'
+import { useState, useEffect } from 'react';
+import Blog from './components/Blog';
+import blogService from './services/blogs';
+import loginService from './services/login';
+
+const errorStyle = {
+  "color": "red"
+};
 
 const App = () => {
-  const [blogs, setBlogs] = useState([])
+  const [blogs, setBlogs] = useState([]);
+  const [newBlog, setNewBlog] = useState('');
+  const [user, setUser] = useState(null);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState(null);
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
-  }, [])
+      setBlogs(blogs)
+    )
+  }, [user]);
+
+  const handleLogin = async event => {
+    event.preventDefault();
+
+    try {
+      const user = await loginService.login({ username, password });
+      blogService.setToken(user.token);
+      setUser(user);
+      setUsername('');
+      setPassword('');
+    } catch (error) {
+      setErrorMessage('Wrong Credentials');
+      setTimeout(() => {
+        setErrorMessage(null);
+      }, 5000);
+    }
+  };
+
+  const handleBlogChange = () => {
+
+  };
+
+  const addBlog = () => {
+
+  };
+
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+      <div>
+        username
+        <input
+          type="text"
+          value={username}
+          name="Username"
+          onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        password
+        <input
+          type="password"
+          value={password}
+          name="Password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type="submit">login</button>
+    </form>
+  );
+
+  const blogForm = () => (
+    <form onSubmit={addBlog}>
+      <input
+        value={newBlog}
+        onChange={handleBlogChange}
+      />
+      <button type="submit">save</button>
+    </form>
+  );
+
 
   return (
     <div>
       <h2>blogs</h2>
-      {blogs.map(blog =>
+      <div>
+        <h4 style={errorStyle}>{errorMessage}</h4>
+      </div>
+
+      {
+        user === null
+          ? loginForm() :
+          <div>
+            <p>{user.username} logged-in</p>
+            {blogForm()}
+          </div>
+      }
+
+      {blogs.length && blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
     </div>
   )
-}
+};
 
-export default App
+export default App;
--- part5/blog-frontend/src/services/blogs.js
+++ part5/blog-frontend/src/services/blogs.js
@@ -1,9 +1,33 @@
-import axios from 'axios'
-const baseUrl = '/api/blogs'
+import axios from 'axios';
+const baseUrl = '/api/blogs';
 
-const getAll = () => {
-  const request = axios.get(baseUrl)
-  return request.then(response => response.data)
-}
+let token = null;
 
-export default { getAll }
+const setToken = newToken => {
+  token = `bearer ${newToken}`;
+};
+
+const getAll = async () => {
+  const config = {
+    headers: { Authorization: token }
+  };
+  const response = await axios.get(baseUrl, config);
+  return response.data;
+};
+
+const create = async newBlog => {
+  const config = {
+    headers: { Authorization: token }
+  };
+
+  const response = await axios.post(baseUrl, newBlog, config);
+  return response.data;
+};
+
+const update = async (id, newBlog) => {
+  const response = await axios.put(`${baseUrl}/${id}`, newBlog);
+  return response.data;
+};
+
+const blogService = { setToken, getAll, create, update };
+export default blogService;
