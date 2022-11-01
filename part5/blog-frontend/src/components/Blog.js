9ae00e3d9ca98145cb3882ee301036a37850e045
Ricky Kuo <rickykuo925@gmail.com>
2022-11-01 11:51:17 +0000

    show more

--- part5/blog-frontend/src/components/Blog.js
+++ part5/blog-frontend/src/components/Blog.js
@@ -1,7 +1,34 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-);
+import { useState } from "react";
+
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false);
+
+  const handleClick = () => {
+    setVisible(!visible);
+  };
+
+  const showAll = () => (
+    <>
+      <p>Title: {blog.title} <button onClick={handleClick}>{visible ? 'hide' : 'view'}</button></p>
+      <p>URL: {blog.url}</p>
+      <p>Likes: {blog.likes}</p>
+      <p>Author: {blog.author}</p>
+    </>
+  );
+
+  const showLess = () => (
+    <>
+      <p>Title: {blog.title} <button onClick={handleClick}>{visible ? 'hide' : 'view'}</button></p>
+      <p>Author: {blog.author}</p>
+    </>
+  );
+
+  return (
+    <div>
+      {visible ? showAll() : showLess()}
+
+    </div>
+  );
+};
 
 export default Blog;
