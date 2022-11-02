489bc26466051a9a869309544fc4ab0a6186eff1
Ricky Kuo <rickykuo925@gmail.com>
2022-11-02 12:20:41 +0000

    sort

--- part5/blog-frontend/src/App.js
+++ part5/blog-frontend/src/App.js
@@ -93,7 +93,7 @@ const App = () => {
             </div>
         }
 
-        {blogs?.length && blogs.map(blog =>
+        {blogs?.length && blogs.sort((a, b) => b.likes - a.likes).map(blog =>
           <Blog key={blog.id} blog={blog} />
         )}
       </React.StrictMode>
