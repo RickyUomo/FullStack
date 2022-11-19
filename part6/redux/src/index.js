05999f8fe1d7e2f5b713cf7f882297c6e8d5ce91
Ricky Kuo <rickykuo925@gmail.com>
2022-11-19 21:20:59 +0000

    add actions creator

--- part6/redux/src/index.js
+++ part6/redux/src/index.js
@@ -1,47 +1,64 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createStore } from 'redux';
+import noteReducer from './reducers/noteReducer';
 
-const counterReducer = (state = 0, action) => {
-  switch (action.type) {
-    case 'INCREMENT':
-      return state + 1
-    case 'DECREMENT':
-      return state - 1
-    case 'ZERO':
-      return 0
-    default: // if none of the above matches, code comes here
-      return state
-  }
-}
-
-const store = createStore(counterReducer);
+const store = createStore(noteReducer);
 store.subscribe(() => {
   const storeNow = store.getState();
   console.log(storeNow);
-})
+});
+
+const generateId = () => Number((Math.random() * 1000000).toFixed(0));
+
+const createNote = (content) => { // action creator
+  return {
+    type: 'NEW_NOTE',
+    data: {
+      content,
+      id: generateId(),
+      important: false
+    }
+  }
+};
+
+const toggleImportant = (id) => {
+  return {
+    type: 'TOGGLE_IMPORTANCE',
+    data: {
+      id
+    }
+  }
+};
 
 const App = () => {
+  const addNote = (event) => {
+    event.preventDefault();
+    const content = event.target.note.value;
+    event.target.note.value = '';
+    store.dispatch(createNote(content));
+  };
+
+  const toggleImportance = (id) => {
+    store.dispatch(toggleImportant(id));
+  };
+
   return (
     <div>
-      <div>
-        {store.getState()}
-      </div>
-      <button
-        onClick={e => store.dispatch({ type: 'INCREMENT' })}
-      >
-        plus
-      </button>
-      <button
-        onClick={e => store.dispatch({ type: 'DECREMENT' })}
-      >
-        minus
-      </button>
-      <button
-        onClick={e => store.dispatch({ type: 'ZERO' })}
-      >
-        zero
-      </button>
+      <form onSubmit={addNote}>
+        <input name="note" />
+        <button type="submit">add</button>
+      </form>
+      <ul>
+        {store.getState().map(note =>
+          <li
+            key={note.id}
+            onClick={() => toggleImportance(note.id)}
+          >
+            {note.content} <strong>{note.important ? 'important' : ''}</strong>
+          </li>
+        )}
+      </ul>
     </div>
   )
 }
